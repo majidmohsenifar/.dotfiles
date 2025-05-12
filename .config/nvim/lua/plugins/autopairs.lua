18cffa76f4dfcfec2f20f8aca20b3b34697950ce
majid <majidmohsenifar@gmail.com>
2025-05-12 15:09:20 +0330

    better autopairs

--- .config/nvim/lua/plugins/autopairs.lua
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -1,9 +1,18 @@
 return {
-  {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    config = function()
-      require("nvim-autopairs").setup()
-    end,
-  },
+{
+  "windwp/nvim-autopairs",
+  event = "InsertEnter",
+  config = function()
+    local npairs = require("nvim-autopairs")
+    local Rule = require("nvim-autopairs.rule")
+
+    npairs.setup({})
+
+    npairs.add_rules({
+    Rule('|', '|', {'rust'}) -- Applies to Lua, Python, and JS files
+})
+  end,
 }
+}
+
+-- Example setup using lazy.nvim
