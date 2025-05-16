22b2b60386f62dd30f09ba0f89de1315adf936d4
majid <majidmohsenifar@gmail.com>
2025-05-16 14:14:49 +0330

    better config for blink

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -6,9 +6,17 @@ return {
   -- use a release tag to download pre-built binaries
   version = '1.*',
   opts = {
-    keymap = { preset = 'default' },
-    signature = { enabled = true },
+    keymap = {
+            preset = 'default',
 
+            ['<C-y>'] = { 'accept', 'fallback_to_mappings' },
+        },
+    signature = { enabled = true },
+    completion = {
+             list = {
+                selection = {preselect = false, auto_insert = true },
+            }
+    },
   },
   opts_extend = { "sources.default" }
 }
